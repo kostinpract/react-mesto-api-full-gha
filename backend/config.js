e1a5123158935b985e0df42664ad745711016d52
Alexey Kostin <alexey-kostin@yandex.ru>
2023-06-15 22:56:26 +0300

    fixes

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-
-const SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'super-strong-secret';
-const { PORT = 3000 } = process.env;
-const { MONGOURI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const prod = process.env.NODE_ENV === 'production';
+const SECRET = prod ? process.env.JWT_SECRET : 'super-strong-secret';
+const { PORT = 3000 } = process.env.PORT;
+const { MONGOURI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env.MONGOURI;
 const REGEXPR = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports = {
