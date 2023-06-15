512485caf87ce2bbc5b58d52f5a61f79120959ee
Alexey Kostin <alexey-kostin@yandex.ru>
2023-06-15 20:26:27 +0300

    feat: add frontend with domain in api config

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.kostinpract.students.nomoredomains.rocks";
 
 const getResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
